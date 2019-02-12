a17ad93b64e0108ab3a69c3eed00450b81953c6f
Bingxiong Yi <yibingxiong@126.com>
2019-02-12 15:08:16 +0800

    fix: 🐛 bug

--- examples/src/pages/Button.jsx
+++ examples/src/pages/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from '../../../src/Button';
 
-export default function Home() {
+export default function ButtonPage() {
   return (
     <div>
       <Button text="按钮" />
